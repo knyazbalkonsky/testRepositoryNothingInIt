3954f0f6f45cd8987104062daf2cbd16dc6fd222
knyazbalkonsky <studentknyazev@gmail.com>
2017-02-16 20:38:55 +0400

    Let's begin

---
+++ com/company/cplayer/src/Core.java
@@ -0,0 +1,7 @@
+/**
+ * Created on 16.02.2017.
+ * At 20:38.
+ * Today is четверг
+ */
+public class Core {
+}
---
+++ com/company/cplayer/src/gui/GuiHandler.java
@@ -0,0 +1,9 @@
+package gui;
+
+/**
+ * Created on 16.02.2017.
+ * At 20:37.
+ * Today is четверг
+ */
+public class GuiHandler {
+}
