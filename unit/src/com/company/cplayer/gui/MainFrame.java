76492df9c2a6a5e14793c15d42affeb2b4c7711d
knyazbalkonsky <studentknyazev@gmail.com>
2017-02-17 11:33:07 +0400

    Update!

--- unit/src/com/company/cplayer/gui/MainFrame.java
+++ unit/src/com/company/cplayer/gui/MainFrame.java
@@ -1,6 +1,7 @@
 package com.company.cplayer.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created on 16.02.2017.
@@ -11,6 +12,13 @@ public class MainFrame extends javax.swing.JFrame {
     public MainFrame(String title){
         super(title);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        add(BorderLayout.CENTER ,new Button("CENTER"));
+        add(BorderLayout.WEST, new Button("WEST"));
+        add(BorderLayout.EAST, new Button("EAST"));
+        add(BorderLayout.NORTH, new Button("NORTH"));
+        add(BorderLayout.SOUTH, new Button("SOUTH"));
+        setSize(600, 600);
     }
 
     public MainFrame(String title, int width, int height){
